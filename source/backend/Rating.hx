82a554b4c0b72f876a7e10f2bccb19e58a600448
Miukothoi2011 <117433468+Miuthoi123@users.noreply.github.com>
2024-04-05 23:02:33 +0700

    0.1.0b4 update again i sorry because i too lazy

--- source/backend/Rating.hx
+++ source/backend/Rating.hx
@@ -11,7 +11,7 @@ class Rating
 	public var score:Int = 500;
 	public var noteSplash:Bool = true;
 	public var hits:Int = 0;
-	var ratingsData:Array<Rating>
+	var ratingsData:Array<Rating>;
 
 	public function new(name:String)
 	{
